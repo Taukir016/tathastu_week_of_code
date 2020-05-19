ca7119fa7da4a629fd42a81dbb2de9178fdeb65c
MOHAMMAD TAUKIR ANSARI <58332167+Taukir016@users.noreply.github.com>
2020-05-19 19:38:42 +0530

    Update program1.py

--- day1/program1.py
+++ day1/program1.py
@@ -1,2 +1,12 @@
-x=int(input("ENTER THE VALUE OF X"))
-print(x)
+name = input("Enter your Name:")
+branch = input("\nEnter your Branch:")
+gender = input("\nEnter your Gender:")
+college = input("\nEnter your College:")
+age = input("\nEnter your Age:")
+
+print("\n\nYour name is:",name)
+print("Your branch is:",branch)
+print("Your gender is:",gender)
+print("Your college is:",college)
+print("Your age is:",age)
+

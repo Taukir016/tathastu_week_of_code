3c82670ef744b204b256d7934f502ebcef6b5a7e
MOHAMMAD TAUKIR ANSARI <58332167+Taukir016@users.noreply.github.com>
2020-05-19 20:48:51 +0530

    Create program5.py

---
+++ day2/program5.py
@@ -0,0 +1,12 @@
+https://www.linkedin.com/in/mohammad-taukir-ansari-734008179
+
+n = int(input("Enter the value of N: "))
+
+print("\n")
+    
+for i in range(n,0,-1):
+    print((str(i) + "*") * (i-1) + str(i))
+    
+for i in range(2,n + 1):
+    print((str(i) + "*") * (i-1) + str(i))
+© 2020 GitHub, Inc.

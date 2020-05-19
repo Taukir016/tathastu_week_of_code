8b897c4d382dbaa87e1a1017ad157a605bfcdd7f
MOHAMMAD TAUKIR ANSARI <58332167+Taukir016@users.noreply.github.com>
2020-05-19 20:00:41 +0530

    Create program3.py

---
+++ day1/program3.py
@@ -0,0 +1,10 @@
+https://www.linkedin.com/in/mohammad-taukir-ansari-734008179
+
+num1 = int(input("Enter 1st number:"))
+num2 = int(input("\nEnter 2nd number:"))
+
+num1 += num2
+num2 = num1-num2
+num1 -= num2
+
+print("\n\nAFTER SWAPPING\n1st number is:",num1,"\n2nd number is:",num2)

48be9ac3655d247e8d7ee57eaee27c87fd99f230
MOHAMMAD TAUKIR ANSARI <58332167+Taukir016@users.noreply.github.com>
2020-05-21 07:08:18 +0530

    Create program1.py

---
+++ day3/program1.py
@@ -0,0 +1,5 @@
+https://www.linkedin.com/in/mohammad-taukir-ansari-734008179
+
+string = input('Enter the string : ')
+print('\nThe original string is :',string)
+print('\nThe reverse of the given string is : ',string[::-1])

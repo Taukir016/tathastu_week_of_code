dbb9f6ef7b3b37b1c64d299a44664694a3f88074
MOHAMMAD TAUKIR ANSARI <58332167+Taukir016@users.noreply.github.com>
2020-05-24 19:42:08 +0530

    Create program2.py

---
+++ day6/program2.py
@@ -0,0 +1,10 @@
+https://www.linkedin.com/in/mohammad-taukir-ansari-734008179
+
+size= int(input("\nEnter size of list:"))
+print("\nEnter list elements(0/1):")
+list=[]
+for i in range(size):
+    list.append(int(input()))
+
+print("Original list={}\nSorted List={}".format(list,sorted(list)))
+© 2020 GitHub, Inc.
